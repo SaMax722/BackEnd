382b492a3acf5c3465ab8bee2659248b95c773e0
KimSangWon <kimsang1220@gmail.com>
2023-08-03 23:00:36 +0900

    java exam10_ commit

---
+++ javastudy/src/exam10_2/GenericsTest5.java
@@ -0,0 +1,11 @@
+package exam10_2;
+
+// <? super E>
+public class GenericsTest5 {
+	public static void main(String[] args) {
+
+	
+		
+	}
+
+}
---
+++ javastudy/src/exam10_3/MapTest2.java
@@ -0,0 +1,35 @@
+package exam10_3;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class MapTest2 {
+
+	public static void main(String[] args) {
+
+		// Map 는 순서가 없다.
+		HashMap<String, String> map2  = new HashMap<String, String>();
+		HashMap<String, String> map  = new HashMap<>();
+		
+		map.put("p1","홍길동");
+		map.put("p2","이순신");
+		map.put("p3","유관순");
+		map.put("p3","강감찬");  // key 중복시 덮어쓴다.
+		
+		
+		// 메서드
+		System.out.println(map.size());
+		map.replace("p3", "세종");
+		
+		map.remove("p3");
+		map.clear();  //전체 삭제
+		
+		// 출력방법 1 - toString
+		System.out.println(map);  
+	
+
+	
+	
+	}
+
+}
